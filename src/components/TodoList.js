e68c41899daacc6863aa1c827de25a51a6373d86
Hazar Koc <hazark41@gmail.com>
2023-04-16 17:45:48 +0300

    font size adjustment
    Signed-off-by: Hazar Koc <hazark41@gmail.com>

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -70,11 +70,11 @@ class TodoList extends Component {
           <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'space-between' }}>
             <span>
               <span className="inline-blocks">
-                <p style={{ fontSize: '2em', margin: '0' }}>{this.state.items.length} </p>
+                <p style={{ fontSize: '3em', margin: '0' }}>{this.state.items.length} </p>
                 <p style={{ margin: '0' }}> &nbsp;&nbsp;&nbsp;TO-DO</p>
               </span>
               <span className="inline-blocks">
-                <p style={{ fontSize: '2em', margin: '0' }}>{completedItemCount} </p>
+                <p style={{ fontSize: '3em', margin: '0' }}>{completedItemCount} </p>
                 <p style={{ margin: '0' }}> &nbsp;&nbsp;&nbsp;Completed</p>
               </span>
             </span>
