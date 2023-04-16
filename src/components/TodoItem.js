0399ec1cab4f06f27761e0db09b9ebd1e88c054e
Hazar Koc <hazark41@gmail.com>
2023-04-16 14:03:11 +0300

    Tıklandığında üstünün çizilmesi ayarlandı. Belirli bir forma oturtuldu.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoItem = ({ text, completed }) => {
+const TodoItem = ({ text, completed, onDelete }) => {
+  const [isClicked, setIsClicked] = useState(false); // yeni state değişkeni
+
+  const handleDelete = () => {
+    onDelete();
+  };
+
+  const handleClick = () => {
+    setIsClicked(!isClicked); // tıklama işlemiyle isClicked değeri tersine çevrilir
+  };
+
   return (
-    <div>
-      <input type="checkbox" checked={completed} />
-      <span>{text}</span>
+    <div 
+      style={{ 
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'space-between', 
+        maxWidth: '700px', // 700px olarak sınırlandırıldı.
+        marginBottom: '10px', // Her öğeden sonra 10px boşluk bırakıldı
+        textDecoration: isClicked ? 'line-through' : 'none' // text üstünün çizilmesi durumunda text-decoration özelliği line-through olarak ayarlanır
+      }}
+      onClick={handleClick} // tıklama işlemi burada uygulanır
+    >
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <input type="checkbox" checked={completed} style={{ marginRight: '5px' }} /> {/* checkbox kutucuğu buraya taşındı ve metinden önce yerleştirildi */}
+        <span>{text}</span> {/* span elementi input kutucuğunun yanına taşındı ve aralarında boşluk bırakıldı */}
+      </div>
+      <span>
+        <button style={{ marginLeft: '200px' }} onClick={handleDelete}>
+          Delete
+        </button>
+      </span>
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,50 +2,75 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoList extends Component {
-  state = {
-    items: [
-      { id: 1, text: 'Test girdisi 1', completed: false },
-      { id: 2, text: 'Araba yıkanacak', completed: false },
-      { id: 3, text: 'Marketten haftalık alışveriş yapılacak', completed: false },
-    ],
-    newItemText: '',
-  };
-
-  handleNewItemChange = (event) => {
-    this.setState({ newItemText: event.target.value });
-  };
-
-  handleNewItemSubmit = (event) => {
-    event.preventDefault();
-    const newItem = {
-      id: Date.now(),
-      text: this.state.newItemText,
-      completed: false,
-    };
-    this.setState({
-      items: [...this.state.items, newItem],
+    state = {
+      items: [
+        { id: 1, text: 'Test girdisi 1', completed: false },
+        { id: 2, text: 'Araba yıkanacak', completed: false },
+        { id: 3, text: 'Marketten haftalık alışveriş yapılacak', completed: false },
+      ],
       newItemText: '',
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>To-Do List</h1>
-        <form onSubmit={this.handleNewItemSubmit}>
-          <input
-            type="text"
-            value={this.state.newItemText}
-            onChange={this.handleNewItemChange}
-          />
-          <button type="submit">Add</button>
-        </form>
-        {this.state.items.map((item) => (
-          <TodoItem key={item.id} text={item.text} completed={item.completed} />
-        ))}
-      </div>
-    );
-  }
-}
+    };
+  
+    handleNewItemChange = (event) => {
+      this.setState({ newItemText: event.target.value });
+    };
+  
+    handleNewItemSubmit = (event) => {
+      event.preventDefault();
+      const newItem = {
+        id: Date.now(),
+        text: this.state.newItemText,
+        completed: false,
+      };
+      this.setState({
+        items: [...this.state.items, newItem],
+        newItemText: '',
+      });
+    };
+  
+    handleDeleteItem = (itemId) => {
+      const updatedItems = this.state.items.filter(item => item.id !== itemId);
+      this.setState({ items: updatedItems });
+    };
+  
+    handleToggleComplete = (itemId) => {
+      const updatedItems = this.state.items.map(item => {
+        if (item.id === itemId) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      });
+      this.setState({ items: updatedItems });
+    };
+  
+    render() {
+      return (
+        <div style={{ marginTop: '50px', display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '700px' }}>
+            <h1>To-Do List</h1>
+            <form onSubmit={this.handleNewItemSubmit}>
+              <input
+                type="text"
+                value={this.state.newItemText}
+                onChange={this.handleNewItemChange}
+              />
+              <button type="submit">Add</button>
+            </form>
+            <div style={{ marginTop: '10px' }}> {/* Input ve listeleme arasında 10px'lik boşluk */}
+              {this.state.items.map((item) => (
+                <TodoItem 
+                  key={item.id} 
+                  text={item.text} 
+                  completed={item.completed} 
+                  onDelete={() => this.handleDeleteItem(item.id)}
+                  onToggleComplete={() => this.handleToggleComplete(item.id)}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }  
 
 export default TodoList;
